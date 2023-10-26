565eb66eb9a7f4aab62b04b3a3a26b183e70093b
alexandr <dmitrymaslov2016@mai.ru>
2023-10-26 23:11:47 +0300

    create db

---
+++ main.py
@@ -0,0 +1,16 @@
+import pathlib
+
+from utils.config import config
+from classes.database import DBManager
+
+dir_path = pathlib.Path.cwd()
+PATH_TO_DB_CONFIG = pathlib.Path(dir_path, 'database.ini')
+
+
+def main():
+    params = config(PATH_TO_DB_CONFIG)
+    db = DBManager('shop', params)
+    print(db.name_db)
+
+if __name__ == '__main__':
+    main()
