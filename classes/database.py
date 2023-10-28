114f9e6d7bb2da1cc74c7000293f59753cc2b5e3
alexandr <dmitrymaslov2016@mai.ru>
2023-10-28 13:29:44 +0300

    create three tables

--- classes/database.py
+++ classes/database.py
@@ -16,3 +16,36 @@ class DBManager:
         with conn.cursor() as cur:
             cur.execute(f"""DROP DATABASE IF EXISTS {self.name_db}""")
             cur.execute(f"""CREATE DATABASE {self.name_db}""")
+
+        with psycopg2.connect(dbname=self.name_db, **self.params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"""CREATE TABLE IF NOT EXISTS shelves (
+                    shelve_id INT PRIMARY KEY,
+                    number VARCHAR(5) UNIQUE NOT NULL);
+                """)
+
+                cur.execute(f"""CREATE TABLE IF NOT EXISTS products (
+                    product_id INT PRIMARY KEY,
+                    title VARCHAR(50),
+                    description VARCHAR(255),
+                    price INT,
+                    count INT,
+                    salary INT
+                    );
+                """)
+
+                cur.execute(f"""CREATE TABLE IF NOT EXISTS shelves_products (
+                    id INT PRIMARY KEY,
+                    shelve_id INTEGER NOT NULL REFERENCES shelves,
+                    product_id   INTEGER NOT NULL REFERENCES products,
+                    UNIQUE (shelve_id, product_id)
+                    );
+                """)
+
+                # cur.execute(f"""CREATE TABLE IF NOT EXISTS orders (
+                #     order_id INT PRIMARY KEY,
+                #     shelve_id INT REFERENCES shelves(shelve_id) NOT NULL,
+                #     product_id INT REFERENCES products(product_id) NOT NULL,
+                #     );
+                # """)
+
